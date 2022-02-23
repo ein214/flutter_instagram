7b3ecaf0c7e6fe9478f0e438c55a6759a81def9e
고은지 <ein214@wizzpay.co.kr>
2022-02-23 12:35:45 +0900

    스크롤위치 파악하는법과 더보기요청

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'style.dart' as style;
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter/rendering.dart';
 
 void main() {
   runApp(
@@ -23,27 +24,68 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   var tab = 0;
   var contents = [];
+  var getHttpCount = 0;
+  var url = "";
+  var scroll = ScrollController();
+  var showAppBar = true;
+
 
   getData() async {
-    try {
-      var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
-      var result2 = jsonDecode(result.body);
-      setState(() {
-        contents = result2;
-      });
-    } catch(error) {
-      Fluttertoast.showToast(
-        msg: "연결에러",
-        timeInSecForIosWeb: 5
-      );
+    switch(getHttpCount) {
+      case 0:
+        url = 'https://codingapple1.github.io/app/data.json';
+        break;
+      case 1:
+        url = 'https://codingapple1.github.io/app/more1.json';
+        break;
+      case 2:
+        url = 'https://codingapple1.github.io/app/more2.json';
+        break;
+      default:
+        url = "";
+        break;
     }
 
+    if (url.isEmpty) {
+      Fluttertoast.showToast(
+          msg: "마지막 게시물입니다.",
+          timeInSecForIosWeb: 5
+      );
+    } else {
+      var result = await http.get(Uri.parse(url));
+      if (result.statusCode == 200) {
+        setState(() {
+          if (getHttpCount == 0) {
+            var result2 = jsonDecode(result.body);
+            print(result2);
+            contents = result2;
+          } else {
+            contents.add(jsonDecode(result.body));
+          }
+          getHttpCount++;
+        });
+      } else {
+        Fluttertoast.showToast(
+            msg: "연결에러",
+            timeInSecForIosWeb: 5
+        );
+      }
+    }
   }
 
   @override
   void initState() {
     super.initState();
     getData();
+    scroll.addListener(() {
+      setState(() {
+        if (scroll.position.userScrollDirection == ScrollDirection.reverse) {
+          showAppBar = false;
+        } else {
+          showAppBar = true;
+        }
+      });
+    });
   }
 
   @override
@@ -55,12 +97,13 @@ class _MyAppState extends State<MyApp> {
             Icon(Icons.add_box_outlined)
           ]
       ),
-      body: [ListTab(contents: contents), Text('샵')][tab],
-      bottomNavigationBar: BottomNavigationBar(
+      body: [ListTab(contents: contents, getData: getData), Text('샵')][tab],
+      bottomNavigationBar: showAppBar == true ? BottomNavigationBar(
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: ''),
           BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), label: ''),
         ],
+
         showSelectedLabels: false,
         showUnselectedLabels: false,
         onTap: (i){
@@ -68,22 +111,43 @@ class _MyAppState extends State<MyApp> {
             tab = i;
           });
         },  //onPressed랑 똑같
-      ),
+      ) : Container(),
 
     );
   }
 }
 
-class ListTab extends StatelessWidget {
-  const ListTab({Key? key, this.contents }) : super(key: key);
+class ListTab extends StatefulWidget {
+  const ListTab({Key? key, this.contents, this.getData }) : super(key: key);
   final contents;
+  final getData;
+
+  @override
+  State<ListTab> createState() => _ListTabState();
+}
+
+class _ListTabState extends State<ListTab> {
+  var scroll = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    scroll.addListener(() {
+      //print(scroll.position.maxScrollExtent);
+      print(scroll.position.userScrollDirection);
+      if (scroll.position.pixels == scroll.position.maxScrollExtent) {
+        widget.getData();
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
 
-    if (contents.isNotEmpty) {
+    if (widget.contents.isNotEmpty) {
       return ListView.builder(
-          itemCount: contents.length,
+          controller: scroll,
+          itemCount: widget.contents.length,
           itemBuilder: (context, index) {
             /**
              * 과제 해답
@@ -107,27 +171,26 @@ class ListTab extends StatelessWidget {
              * - 나는 Container부터 짰는데 Column부터 짜서 이미지 일단 넣고 시작하는게 훨씬 깔끔해보이고
              * - 일단 이미지 가로폭을 늘리려고 했는데 BoxConstraints를 쓰면 최대 폭 자체를 지정할 수가 있었음.
              */
-            return Container(
-              width: double.infinity,
-              child: Column(
-                children: [
-                  Image.network(contents[index]['image']),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text("좋아요 " + contents[index]['likes'].toString(),
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold
-                          ),),
-                        Text(contents[index]['user']),
-                        Text(contents[index]['content']),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+            return Column(
+              children: [
+                Image.network(widget.contents[index]['image']),
+                Container(
+                  constraints: BoxConstraints(maxWidth: double.infinity),
+                  padding: EdgeInsets.all(20),
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("좋아요 ${widget.contents[index]['likes'].toString()}",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold
+                        ),),
+                      Text(widget.contents[index]['user']),
+                      Text(widget.contents[index]['content']),
+                    ],
+                  ),
+                )
+              ],
             );
           }
       );
